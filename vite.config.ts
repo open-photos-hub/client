6436b290af10da68d57e12cf67d60ddec6db41e0
Benjamin Beganović <k1pstabug@gmail.com>
2023-12-25 23:47:10 +0100

    Configure paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import tsconifgPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [tsconifgPaths(), solid()],
 });
